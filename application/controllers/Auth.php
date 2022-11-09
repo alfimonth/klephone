3bf72e92932bbaf2e57b19404cfdcdad4fcefb77
JimiMontini <jimimontini45@gmail.com>
2022-11-09 17:13:25 +0700

    Menambahkan controller auth

---
+++ application/controllers/Auth.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller 
+    { 
+        public function index()
+        {
+            $this->load->view('auth/login');
+        }
+    }
