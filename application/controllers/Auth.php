1f81a59a9ab37353bab751c694475f226bed1453
alfimonth <alvy1610@gmail.com>
2022-11-17 19:45:02 +0700

    membuat function login dan logout

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,10 +1,78 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Auth extends CI_Controller 
-    { 
-        public function index()
-        {
+class Auth extends CI_Controller
+{
+    public function index()
+    {
+        //jika statusnya sudah login, maka tidak bisa mengakses halaman login alias dikembalikan ke tampilan user 
+        if ($this->session->userdata('email')) {
+            redirect('home');
+        }
+
+        $this->form_validation->set_rules('email', 'Alamat Email', 'required|trim|valid_email', ['required' => 'Email Harus diisi!!', 'valid_email' => 'Email Tidak Benar!!']);
+        $this->form_validation->set_rules('password', 'Password', 'required|trim', ['required' => 'Password Harus diisi']);
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Login';
+            $this->load->view('templates/auth_header', $data);
             $this->load->view('auth/login');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $this->_login();
         }
     }
+
+    private function _login()
+    {
+        $email = htmlspecialchars($this->input->post('email', true));
+        $password = $this->input->post('password', true);
+        $user = $this->ModelUser->cekData(['email' => $email])->row_array(); //jika usernya ada 
+        if ($user) { //jika user sudah aktif 
+            if ($user['is_active'] == 1) { //cek password 
+                if (password_verify($password, $user['password'])) {
+                    $data = [
+                        'email' => $user['email'],
+                        'role_id' => $user['role_id']
+                    ];
+
+                    $this->session->set_userdata($data);
+                    if ($user['role_id'] == 1) {
+                        redirect('home');
+                    } else {
+                        if ($user['image'] == 'default.jpg') {
+                            $this->session->set_flashdata('pesan', '<div class="alert alert-info alert-message" role="alert">Silahkan Ubah Profile Anda untuk Ubah Photo Profil</div>');
+                        }
+                        redirect('home');
+                    }
+                } else {
+                    $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Password salah!!</div>');
+                    redirect('auth');
+                }
+            } else {
+                $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">User belum diaktifasi!!</div>');
+                redirect('auth');
+            }
+        } else {
+            $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Email tidak terdaftar!!</div>');
+            redirect('auth');
+        }
+    }
+
+    public function register()
+    {
+        $data['title'] = 'Login';
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('auth/register');
+        $this->load->view('templates/auth_footer');
+    }
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+        $this->session->set_flashdata('pesan', '<div class="alert alert-message alert-success" role="alert">Berhasil logout</div>');
+
+        redirect('auth');
+    }
+}
