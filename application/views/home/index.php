31378617380baf1949ba395e5eb47284dde66870
siihasann <mashasanr@gmail.com>
2022-12-02 10:33:30 +0700

    membuat halaman user

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,9 +1,42 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
+    <div class="jumbotron">
+        <div class="row">
+            <div class="col-6">
+                <h1 class="display-4">Hello, world!</h1>
+                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr class="my-4">
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p class="lead">
+                    <a class="btn btn-success btn-lg" href="#" role="button">Learn more</a>
+                </p>
+            </div>
+            <div class="col-6">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block" src="http://tradeoptiontech.com/wp-content/uploads/2021/11/swappie-product-iphone-11-purple.png" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block" src="https://www.yatekno.com/wp-content/uploads/2020/12/Apple-iPhone-12-Pro-Max-Yatekno.jpg" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block" src="https://media.pricebook.co.id/images/product/L/87099_L_1.jpg" alt="Third slide">
+                        </div>
+                    </div>
+                    <img src=" <?= base_url('assets/img/promo/stand.png'); ?> " class="d-block standrak" alt="" />
+                    <a class="carousel-control-prev prevhome" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next nexthome" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
 
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Daftar Produk Klephone</h1>
+            </div>
+        </div>
     </div>
 
 
