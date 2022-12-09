b9bcc00760a9f78d8efaa286f8a4b9dc0af54707
siihasann <mashasanr@gmail.com>
2022-12-09 13:16:22 +0700

    megganti link css

--- application/views/templates/auth_header.php
+++ application/views/templates/auth_header.php
@@ -18,7 +18,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
 
     <!-- Custom styles for this template-->
-    <link href="<?= base_url() ?>assets/css/sb-admin-2.min.css" rel="stylesheet" />
+    <link href="<?= base_url() ?>assets/css/sba-dmin-3.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/auth.css">
     <link href="<?= base_url('assets/') ?>css/main6.css" rel="stylesheet">
 </head>
